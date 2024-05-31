55595d341be891a4996f0a256c77684ecb943cc9
Bhanugidugu7 <bhanugidugu@gmail.com>
2024-05-31 17:18:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+import { MantineProvider } from '@mantine/core';
 import logo from './logo.svg';
-import './App.css';
-
+import '@mantine/core/styles.css';
+import MyForm from './Components/MyForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <MantineProvider>
+      <MyForm></MyForm>
+     </MantineProvider>
+        
     </div>
   );
 }
